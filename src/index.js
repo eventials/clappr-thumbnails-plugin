0caee6e1461bbdb42c3b9be87f37c00089e44cc8
Luan Raithz <luanraithz@gmail.com>
2019-06-25 15:13:00 -0300

    initial, clonning repo

---
+++ src/index.js
@@ -0,0 +1,146 @@
+import {UICorePlugin, Styler, Events, template, $} from 'clappr'
+import {Promise} from 'es6-promise-polyfill'
+import pluginHtml from './public/thumbnails-plugin.html'
+import pluginStyle from './public/style.sass'
+
+export default class ScrubThumbnailsPlugin extends UICorePlugin {
+
+  get name() { return 'thumbnails-plugin' }
+
+  get attributes() {
+    return {
+      'class': this.name
+    }
+  }
+  get template() { return template(pluginHtml) }
+
+  constructor(core) {
+    super(core)
+    this.show = false
+    this.hoverPosition = 0
+    this._oldContainer = null
+    this.parent = this.$el.parent()
+  }
+
+  bindEvents() {
+    // Clappr 0.3 support
+    if (Events.CORE_ACTIVE_CONTAINER_CHANGED) {
+      this.listenTo(this.core, Events.CORE_ACTIVE_CONTAINER_CHANGED, this.rebindEvents)
+    }
+    this.listenTo(this.core.mediaControl, Events.MEDIACONTROL_MOUSEMOVE_SEEKBAR, this.onMouseMove)
+    this.listenTo(this.core.mediaControl, Events.MEDIACONTROL_MOUSELEAVE_SEEKBAR, this.onMouseLeave)
+    this.listenTo(this.core.mediaControl, Events.MEDIACONTROL_RENDERED, this.init)
+    this.listenTo(this.core.mediaControl, Events.MEDIACONTROL_CONTAINERCHANGED, this.bindContainerEvents)
+  }
+
+  rebindEvents() {
+    this.stopListening()
+    this.bindEvents()
+  }
+
+  bindContainerEvents() {
+    if (this._oldContainer) {
+      this.stopListening(this._oldContainer, Events.CONTAINER_TIMEUPDATE, this.renderPlugin)
+    }
+    this._oldContainer = this.core.mediaControl.container
+    this.listenTo(this.core.mediaControl.container, Events.CONTAINER_TIMEUPDATE, this.renderPlugin)
+  }
+
+  init() {
+    this._createElements()
+    this.renderPlugin()
+  }
+
+  getOptions() {
+    if (!("scrubThumbnails" in this.core.options)) {
+      throw "'scrubThumbnails property missing from options object."
+    }
+    return this.core.options.scrubThumbnails
+  }
+
+  appendElToMediaControl() {
+    // insert after the background
+    this.core.mediaControl.$el.find(".media-control-background").first().after(this.el)
+  }
+
+  onMouseMove(e) {
+    this.calculateHoverPosition(e)
+    this.show = true
+    this.renderPlugin()
+  }
+
+  onMouseLeave() {
+    this.show = false
+    this.renderPlugin()
+  }
+
+  calculateHoverPosition(e) {
+    const offset = e.pageX - this.core.mediaControl.$seekBarContainer.offset().left
+    // proportion into the seek bar that the mouse is hovered over 0-1
+    this.overPosition = Math.min(1, Math.max(offset/this.core.mediaControl.$seekBarContainer.width(), 0))
+  }
+
+  buildImg(thumbUrl) {
+    const $img = $("<img />").addClass("thumbnail-img").attr("src", thumbUrl)
+
+    // the container will contain the image positioned so that the correct sprite
+    // is visible
+    const $container = $("<div />").addClass("thumbnail-container")
+    $container.css("height", '150px')
+    $container.append($img)
+    return $container
+  }
+
+  // calculate how far along the carousel should currently be slid
+  // depending on where the user is hovering on the progress bar
+
+  updateSpotlightThumb() {
+    const { hoverPosition, $spotlight, previousTime } = this
+    const videoDuration = this.core.mediaControl.container.getDuration()
+    // the time into the video at the current hover position
+    const startTimeOffset = this.core.mediaControl.container.getStartTimeOffset()
+    const hoverTime = Math.floor((startTimeOffset + (videoDuration * hoverPosition)) / 5) * 5
+
+    const elWidth = this.$el.width()
+    const thumbWidth = $spotlight.width()
+
+    const spotlightXPos = Math.max(Math.min((elWidth * hoverPosition) - (thumbWidth / 2), elWidth - thumbWidth), 0)
+
+    $spotlight.css("left", spotlightXPos)
+    if (hoverTime === previousTime) return
+
+    this.previousTime = hoverTime
+
+    const thumbUrl = this.core.options.scrubThumbnails.getThumbnailForTime(hoverTime)
+
+    $spotlight.empty()
+    $spotlight.append(this.buildImg(thumbUrl, this.getOptions().spotlightHeight))
+
+  }
+
+  renderPlugin() {
+    if (this.show) {
+      if (!this.parent.length) {
+        this.parent = this.$el.parent()
+      }
+      if (this.parent[0]) {
+        this.parent[0].appendChild(this.$el[0])
+        this.updateSpotlightThumb()
+      }
+    } else {
+        if (this.$el.parent()[0]) {
+            // first time the parent doesn't exist but the `$el.parent` does
+            this.parent = this.$el.parent()
+            this.parent[0].removeChild(this.$el[0])
+        }
+    }
+  }
+
+  _createElements() {
+    this.$el.html(this.template())
+    this.$el.append(Styler.getStyleFor(pluginStyle))
+
+    this.$spotlight = this.$el.find(".spotlight")
+    this.appendElToMediaControl()
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var path = require('path');
+var webpack = require('webpack');
+var Clean = require('clean-webpack-plugin');
+
+var plugins = [
+  new Clean(['dist'])
+];
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  plugins: plugins,
+  externals: {
+    clappr: {
+      amd: 'clappr',
+      commonjs: 'clappr',
+      commonjs2: 'clappr',
+      root: 'Clappr'
+    }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        query: {
+          compact: true,
+        }
+      },
+      {
+        test: /\.sass$/,
+        loaders: ['css', 'sass?includePaths[]=' + path.resolve(__dirname, "./node_modules/compass-mixins/lib")],
+      },
+      {
+        test: /\.html/, loader: 'html?minimize=false'
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'clappr-thumbnails-plugin.js',
+    library: 'ClapprThumbnailsPlugin',
+    libraryTarget: 'umd',
+  },
+};
